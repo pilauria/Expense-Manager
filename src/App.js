abc24a7f9b94b6241145d463a681d3679ec358cc
Pietro Lauria <ptr.lauria@gmail.com>
2022-03-06 21:04:19 +0100

    end react part

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
-import Transanction from './components/Transanction';
+import TransanctionList from './components/TransanctionList';
 import AddTransaction from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
         <IncomeExpense />
-        <Transanction />
+        <TransanctionList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
---
+++ src/context/AppReducer.js
@@ -0,0 +1,23 @@
+// Here we specify the app state changes in response to certain actions to our store (to our context). CHange the state and sent it down to a component (creating a new state-with the spread operator- and send it down)
+
+export default (state, action) => {
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          /// ***
+          transaction => transaction.id !== action.payload
+        ),
+      };
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
+};
+
+// *** send all the transaction except the one that was deleted
